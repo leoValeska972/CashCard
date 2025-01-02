e2770314fa8edd4e123eb8272a23e12564737c1c
Zaw Lwin Tun <zawlwintun1412@gmail.com>
2025-01-02 22:40:00 +0630

    Module 2 : Developing a Secure App (Lab: Returning a list with GET)
    - lest the new Cash Card "list" JSON data contract
    - implement a GET endpoint for a list
    - implement pagination
    - implement sorting
    - combine paging and sorting and add default behavior
    - learn about test interaction and the @DirtiesContext annotation

--- src/main/java/com/leo/cashcard/CashCardRepository.java
+++ src/main/java/com/leo/cashcard/CashCardRepository.java
@@ -1,9 +1,10 @@
 package com.leo.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 
 }
